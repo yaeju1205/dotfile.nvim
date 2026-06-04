c47c450476d763aa286d7bc8aecbb12755d79363
yaeju <gimleni593@gmail.com>
2026-06-04 18:31:06 +0900

    Add statusline

--- lua/yaeju/plugins/search.lua
+++ lua/yaeju/plugins/search.lua
@@ -28,6 +28,46 @@ vim.plugin.namespace("yaeju-search", function()
     vim.plugin.install("hedyhli/outline.nvim")(function()
         vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
 
-        require("outline").setup()
+        require("outline").setup({
+            symbols = {
+                filter = nil,
+                icon_source = nil,
+                icon_fetcher = nil,
+                icons = {
+                    File =          { icon = "󰈔", hl = "Identifier" },
+                    Module =        { icon = "󰆧", hl = "Include" },
+                    Namespace =     { icon = "󰅪", hl = "Include" },
+                    Package =       { icon = "󰏗", hl = "Include" },
+                    Class =         { icon = "", hl = "Type" },
+                    Method =        { icon = "", hl = "Function" },
+                    Property =      { icon = "", hl = "Identifier" },
+                    Field =         { icon = "󰆨", hl = "Identifier" },
+                    Constructor =   { icon = "", hl = "Special" },
+                    Enum =          { icon = "ℰ", hl = "Type" },
+                    Interface =     { icon = "󰜰", hl = "Type" },
+                    Function =      { icon = "󰆧", hl = "Function" },
+                    Variable =      { icon = "󰆧", hl = "Constant" },
+                    Constant =      { icon = "󰆧", hl = "Constant" },
+                    String =        { icon = "𝓐", hl = "String" },
+                    Number =        { icon = "#", hl = "Number" },
+                    Boolean =       { icon = "⊨", hl = "Boolean" },
+                    Array =         { icon = "󰅪", hl = "Constant" },
+                    Object =        { icon = "⦿", hl = "Type" },
+                    Key =           { icon = "🔐", hl = "Type" },
+                    Null =          { icon = "NULL", hl = "Type" },
+                    EnumMember =    { icon = "", hl = "Identifier" },
+                    Struct =        { icon = "", hl = "Structure" },
+                    Event =         { icon = "🗲", hl = "Type" },
+                    Operator =      { icon = "+", hl = "Identifier" },
+                    TypeParameter = { icon = "𝙏", hl = "Identifier" },
+                    Component =     { icon = "󰅴", hl = "Function" },
+                    Fragment =      { icon = "󰅴", hl = "Constant" },
+                    TypeAlias =     { icon = " ", hl = "Type" },
+                    Parameter =     { icon = " ", hl = "Identifier" },
+                    StaticMethod =  { icon = " ", hl = "Function" },
+                    Macro =         { icon = " ", hl = "Function" },
+                },
+            },
+        })
     end)
 end)
---
+++ lua/yaeju/plugins/statusline.lua
@@ -0,0 +1,41 @@
+vim.plugin.namespace("yaeju-statusline", function()
+    vim.plugin.install("nvim-tree/nvim-web-devicons")(function()
+        local web_devicons = require("nvim-web-devicons")
+
+        local function highlight_string(hl, str)
+            return "%#" .. hl .. "#" .. str .. "%*"
+        end
+
+        vim.statusline = {}
+        vim.statusline.render = function()
+            local file_name = vim.fn.expand("%:t")
+            local file_icon, file_icon_hl = web_devicons.get_icon(file_name, nil, { default = true })
+
+            local file_type = vim.bo.filetype
+            if file_type == "" then
+                file_type = "text"
+            end
+
+            local file_icon_text = highlight_string(file_icon_hl, file_icon or "")
+            local file_path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":.")
+
+            local diag_count = vim.diagnostic.count()
+            return table.concat({
+                "  ",
+                file_path,
+                "%m%r",
+                "%=",
+                highlight_string("DiagnosticError", "  " .. (diag_count[1] or 0)),
+                "   ",
+                highlight_string("DiagnosticWarn", "  " .. (diag_count[2] or 0)),
+                "  ",
+                "| " .. file_icon_text .. " " .. file_type .. " ",
+                "| %p%% ",
+                "| %l:%c",
+                "  ",
+            })
+        end
+
+        vim.opt.statusline = "%!v:lua.vim.statusline.render()"
+    end)
+end)
